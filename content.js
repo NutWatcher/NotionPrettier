8edf0a820b9a6e44e1550f09957a8bb670de894b
NutWatcher <lyy@nutwatcher.com>
2024-07-12 11:09:29 +0800

    fix

--- content.js
+++ content.js
@@ -9,10 +9,10 @@ if (NotionCard_notionContent) {
   NotionCard_notionContent.style.paddingBottom = '38px';
   NotionCard_notionContent.style.borderRadius = '24px';
 
-  var parentElement = NotionCard_notionContent.parentElement;
-  if (parentElement) {
-    parentElement.style.padding = '50px';
-    parentElement.style.backgroundImage = 'linear-gradient(100deg, rgb(204, 251, 252), rgb(197, 234, 254), rgb(189, 211, 255))';
+  var NotionCard_parentElement = NotionCard_notionContent.parentElement;
+  if (NotionCard_parentElement) {
+    NotionCard_parentElement.style.padding = '50px';
+    NotionCard_parentElement.style.backgroundImage = 'linear-gradient(100deg, rgb(204, 251, 252), rgb(197, 234, 254), rgb(189, 211, 255))';
   } else {
     console.error("Parent element not found.");
   }

1d4b30c88ba926df960f30ddd4f8ae113fde0a44
NutWatcher <lyy@nutwatcher.com>
2024-07-12 08:46:14 +0800

    fix bugs

--- content.js
+++ content.js
@@ -1,14 +1,15 @@
-const notionContent = document.querySelector('.notion-page-content');
-if (notionContent) {
-  notionContent.style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
-  notionContent.style.boxShadow = '4px 4px 50px -20px #00000080';
-  notionContent.style.paddingTop = '38px';
-  notionContent.style.paddingLeft = '38px';
-  notionContent.style.paddingRight = '38px';
-  notionContent.style.paddingBottom = '38px';
-  notionContent.style.borderRadius = '24px';
+const NotionCard_notionContent = document.querySelector('.notion-page-content');
+console.log(NotionCard_notionContent);
+if (NotionCard_notionContent) {
+  NotionCard_notionContent.style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
+  NotionCard_notionContent.style.boxShadow = '4px 4px 50px -20px #00000080';
+  NotionCard_notionContent.style.paddingTop = '38px';
+  NotionCard_notionContent.style.paddingLeft = '38px';
+  NotionCard_notionContent.style.paddingRight = '38px';
+  NotionCard_notionContent.style.paddingBottom = '38px';
+  NotionCard_notionContent.style.borderRadius = '24px';
 
-  const parentElement = notionContent.parentElement;
+  const parentElement = NotionCard_notionContent.parentElement;
   if (parentElement) {
     parentElement.style.padding = '50px';
     parentElement.style.backgroundImage = 'linear-gradient(100deg, rgb(204, 251, 252), rgb(197, 234, 254), rgb(189, 211, 255))';
@@ -16,5 +17,5 @@ if (notionContent) {
     console.error("Parent element not found.");
   }
 } else {
-  console.error("Notion page content not found.");
+  alert("Notion Card:  Unable to locate content in the current page of Notion document, please try refreshing.");
 }

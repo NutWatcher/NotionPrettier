9ba1ac588300e5d7a58187e087618dcd8efbd5c0
NutWatcher <lyy@nutwatcher.com>
2024-07-12 11:07:24 +0800

    修复 const 变量重复定义

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const NotionCard_notionContent = document.querySelector('.notion-page-content');
+var NotionCard_notionContent = document.querySelector('.notion-page-content');
 console.log(NotionCard_notionContent);
 if (NotionCard_notionContent) {
   NotionCard_notionContent.style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
@@ -9,7 +9,7 @@ if (NotionCard_notionContent) {
   NotionCard_notionContent.style.paddingBottom = '38px';
   NotionCard_notionContent.style.borderRadius = '24px';
 
-  const parentElement = NotionCard_notionContent.parentElement;
+  var parentElement = NotionCard_notionContent.parentElement;
   if (parentElement) {
     parentElement.style.padding = '50px';
     parentElement.style.backgroundImage = 'linear-gradient(100deg, rgb(204, 251, 252), rgb(197, 234, 254), rgb(189, 211, 255))';

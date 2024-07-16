dbe18edb9fcf1232adcf12062a4812e97ba06c69
NutWatcher <lyy@nutwatcher.com>
2024-07-16 16:48:57 +0800

    1.1.0版本，增加了多个颜色可选

--- content.js
+++ content.js
@@ -1,21 +1,46 @@
-var NotionCard_notionContent = document.querySelector('.notion-page-content');
-// console.log(NotionCard_notionContent);
-if (NotionCard_notionContent) {
-  NotionCard_notionContent.style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
-  NotionCard_notionContent.style.boxShadow = '4px 4px 50px -20px #00000080';
-  NotionCard_notionContent.style.paddingTop = '38px';
-  NotionCard_notionContent.style.paddingLeft = '38px';
-  NotionCard_notionContent.style.paddingRight = '38px';
-  NotionCard_notionContent.style.paddingBottom = '38px';
-  NotionCard_notionContent.style.borderRadius = '24px';
+var Notion_Card_Style = {
+    style1: "linear-gradient(320deg, #dfe9f3 0%, white 100%)",
+    style2: "linear-gradient(-225deg, #D4FFEC 0%, #57F2CC 48%, #4596FB 100%",
+    style3: "linear-gradient(120deg, #a18cd1 0%, #fbc2eb 100%)",
+    style4: "linear-gradient(100deg, rgb(204, 251, 252), rgb(197, 234, 254), rgb(189, 211, 255))",
+    style5: "linear-gradient(105deg, rgb(245, 159, 156), rgb(69, 165, 215))",
+    style6: "linear-gradient(200deg, #30cfd0 0%, #330867 100%)",
+    style7: "linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%)",
+    style8: "linear-gradient(120deg, #f6d365 0%, #fda085 100%)",
+    style9: "linear-gradient(120deg, #f43b47 0%, #453a94 100%)",
+};
 
-  var NotionCard_parentElement = NotionCard_notionContent.parentElement;
-  if (NotionCard_parentElement) {
-    NotionCard_parentElement.style.padding = '50px';
-    NotionCard_parentElement.style.backgroundImage = 'linear-gradient(100deg, rgb(204, 251, 252), rgb(197, 234, 254), rgb(189, 211, 255))';
-  } else {
-    console.error("Parent element not found.");
-  }
-} else {
-  alert("Notion Card:  Unable to locate content in the current page of Notion document, please try refreshing.");
-}
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    var style = message.style;
+    var NotionCard_notionContent = document.querySelector(
+        ".notion-page-content"
+    );
+    if (NotionCard_notionContent) {
+        NotionCard_notionContent.style.backgroundColor =
+            "rgba(255, 255, 255, 0.4)";
+        NotionCard_notionContent.style.boxShadow =
+            "4px 4px 50px -20px #00000080";
+        NotionCard_notionContent.style.paddingTop = "38px";
+        NotionCard_notionContent.style.paddingLeft = "38px";
+        NotionCard_notionContent.style.paddingRight = "38px";
+        NotionCard_notionContent.style.paddingBottom = "38px";
+        NotionCard_notionContent.style.borderRadius = "24px";
+        NotionCard_notionContent.style.boxShadow =
+            "0 10px 40px hsla(0,0%,39%,.4)";
+
+        var NotionCard_parentElement = NotionCard_notionContent.parentElement;
+        if (NotionCard_parentElement) {
+            NotionCard_parentElement.style.padding = "58px";
+            NotionCard_parentElement.style.backgroundImage =
+                "linear-gradient(100deg, rgb(204, 251, 252), rgb(197, 234, 254), rgb(189, 211, 255))";
+        } else {
+            console.error("Parent element not found.");
+        }
+        NotionCard_parentElement.style.backgroundImage =
+            Notion_Card_Style[style];
+    } else {
+        alert(
+            "Notion Card:  Unable to locate content in the current page of Notion document, please try refreshing."
+        );
+    }
+});
--- popup.js
+++ popup.js
@@ -1,12 +1,40 @@
-// popup.js
-document.getElementById('changeBackground').addEventListener('click', () => {
-    console.log(111);
+document
+    .getElementById("style1")
+    .addEventListener("click", () => applyStyle("style1"));
+document
+    .getElementById("style2")
+    .addEventListener("click", () => applyStyle("style2"));
+document
+    .getElementById("style3")
+    .addEventListener("click", () => applyStyle("style3"));
+document
+    .getElementById("style4")
+    .addEventListener("click", () => applyStyle("style4"));
+document
+    .getElementById("style5")
+    .addEventListener("click", () => applyStyle("style5"));
+document
+    .getElementById("style6")
+    .addEventListener("click", () => applyStyle("style6"));
+document
+    .getElementById("style7")
+    .addEventListener("click", () => applyStyle("style7"));
+document
+    .getElementById("style8")
+    .addEventListener("click", () => applyStyle("style8"));
+document
+    .getElementById("style9")
+    .addEventListener("click", () => applyStyle("style9"));
+function applyStyle(style) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        console.log(tabs);
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        files: ['content.js']
-      });
+        chrome.scripting.executeScript(
+            {
+                target: { tabId: tabs[0].id },
+                files: ["content.js"],
+            },
+            () => {
+                chrome.tabs.sendMessage(tabs[0].id, { style: style });
+            }
+        );
     });
-  });
-  
+}
